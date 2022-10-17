ea04a8e2b7ef2f0e55068e3007c5e08593573b3f
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-17 12:01:08 +0530

    operators : all operators

---
+++ 01_day/operators.py
@@ -0,0 +1,31 @@
+num1 = 10
+num2 = 20
+
+print(f'Add is {num1 + num2}')
+print(f'Sub is {num1 - num2}')
+print(f'Mul is {num1 * num2}')
+print(f'Div is {num1 / num2}')
+print(f'Div is {num1 ** 3}')
+print(f'Left Shift is {num1 << 3}')
+print(f'Bitwise And {num1 & 3}')
+print(f'Bitwise Or {num1 | 3}')
+print(f'Bitwise xOr {num1 ^ 3}')
+print(f'GT {num1 > 3}')
+print(f'LTE {num1 <= 3}')
+print(f'Eq {num1 == 3}')
+print(f'NEq {num1 != 3}')
+num1 = num1 * 3
+print(f'New Num1 {num1}' )
+num1 *= 3
+print(f'New Num1 {num1}' )
+
+ln = 5
+br = 2
+ar = ln * br
+print('Area is ', ar)
+print(f'Perimeter is {2 * (ln + br)}')
+
+# 2+3+4
+# (2+3) + 4
+
+
