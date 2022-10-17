6508cdd013b5a65a495ad0ae763247e9dde5f106
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-17 11:10:20 +0530

    variables : type and values

---
+++ 01_day/variables.py
@@ -0,0 +1,14 @@
+print('🔥Lets work with variables')
+
+population = 10
+isCountry = True
+countryName = "abc"
+
+print('Type is ' + str(type(population)))
+print(f'Type is {type(population)}')
+print(f'Type is {type(isCountry)}')
+print(f'Type is {type(countryName)}')
+
+population = '10 lakh'
+isCountry = 'YES'
+countryName = 123
