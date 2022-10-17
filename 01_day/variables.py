6cafa9fead2edeb24fe9f7a91c97d8b8588e8d01
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-17 11:17:06 +0530

    vaiables : identifier naming

--- 01_day/variables.py
+++ 01_day/variables.py
@@ -4,6 +4,8 @@ population = 10
 isCountry = True
 countryName = "abc"
 
+
+print('🔥 Data Types')
 print('Type is ' + str(type(population)))
 print(f'Type is {type(population)}')
 print(f'Type is {type(isCountry)}')
@@ -12,3 +14,12 @@ print(f'Type is {type(countryName)}')
 population = '10 lakh'
 isCountry = 'YES'
 countryName = 123
+
+print('🔥 Identifier Naming')
+# 1223_abc = 10
+countryname = 'abc'
+country_name = 'abc'
+_contry_name_ = 'abc';
+contry_1234 = 'abc';
+# contry_$% = 'abc'
+
