f6fc025bfd63c26fa6cc9d1e488804473ebbc417
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-17 15:21:05 +0530

    variables : static type checking and type casting

---
+++ 01_day/variables1.py
@@ -0,0 +1,13 @@
+print('🔥Variables Advanced')
+
+a = 10.7
+print(type(a))
+b = int(a)
+print(type(b))
+num = '91'
+print(type(num))
+c = int(num)
+print(type(c))
+
+
+
---
+++ 01_day/variables2.py
@@ -0,0 +1,18 @@
+import re
+
+
+print('🔥 Static Typing')
+
+address: str = 'abc'
+# address = 90
+
+def clarity() -> int:
+    return 10
+
+def traditional() -> bool:
+    # return 10
+    return 'hi'
+    # return True
+
+def add(num1: int, num2: int) -> int:
+    return num1 + num2
