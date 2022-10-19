846e59a4d7ba8256cbb808076e80e5aed26c47a6
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-19 18:00:06 +0530

    case study implementation

--- cli/menu/menurunner.py
+++ cli/menu/menurunner.py
@@ -1,4 +1,7 @@
 import menu.admin as admin
+import menu.customer as customer
+import menu.first as first
+
 from ipdata.appinput import AppInput
 
 class MenuRunner:
@@ -7,12 +10,25 @@ class MenuRunner:
         self.ip = AppInput()
 
     def show_menu(self):
+        first.show_menu()
+
         while True:
-            admin.show_menu()
             ch = self.ip.user_choice()
             print(f'User Choice {ch}')
             if ch >= 3:
                 exit(1)
+            if ch == 1 :
+                admin.show_menu()
+                while True:
+                    ad_ch = self.ip.user_choice()
+                    if ad_ch >= 3:
+                        break
+                    if ad_ch == 1:
+                        print('creating new customer')
+                    if ad_ch == 2:
+                         print('here are the customers')
+            if ch == 2:
+                customer.show_menu()
 
 
 
