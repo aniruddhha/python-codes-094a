d7d78d3d1bfa9d944fbe50530aa0ee8d1ed1b414
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-26 11:43:48 +0530

    crud app : update record

--- 06_day/crudmain/crudrest/views.py
+++ 06_day/crudmain/crudrest/views.py
@@ -32,5 +32,16 @@ class AppUserView(APIView):
 
         return Response({ 'sts' : 'success', 'msg' : 'user saved successfully' })
 
-    def put(self, request): ...
+    def put(self, request): 
+        req_data = request.data
+
+        app_user = AppUser.objects.get(id = req_data['id'])
+        app_user.first_name = req_data['first_name']
+        app_user.last_name = req_data['last_name']
+        app_user.email = req_data['email']
+        app_user.save(update_fields=['first_name', 'last_name', 'email'])
+
+        return Response({ 'sts' : 'success', 'msg' : 'user updated successfully' })
+
+
     def delete(self, request): ...
