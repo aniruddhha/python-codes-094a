d5f722fbab2b92bd9edda5d0e950656a707dc9f7
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-26 15:15:26 +0530

    crud : viewset and routers

--- 06_day/crudmain/crudrest/urls.py
+++ 06_day/crudmain/crudrest/urls.py
@@ -12,8 +12,7 @@ from .views import (
 )
 
 
-router = DefaultRouter()
-router.register(r'appuser', AppUserViewSet, basename='usr')
+
 # router.register('appadmin', AdminView.as_view())
 # router.register('features', FeaturesView.as_view())
 
@@ -23,6 +22,8 @@ router.register(r'appuser', AppUserViewSet, basename='usr')
 #     path('<int:id>/', AppUserDeatilsView.as_view()), #http://localhost:8000/1
 # ]
 
+router = DefaultRouter()
+router.register(r'appuser', AppUserViewSet, basename='usr')
 urlpatterns = [ 
     path('', include(router.urls) )
- ]
+]
