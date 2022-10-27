e138be2f96537f152d6c03ba8149a6f070d9507e
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-27 17:06:20 +0530

    pytest : testing rest api

--- 07_day/asyncdemo/asyncapp/my_tests.py
+++ 07_day/asyncdemo/asyncapp/my_tests.py
@@ -1,5 +1,11 @@
 from cmath import exp
+from pydoc import cli
 from .deco import add
+import pytest
+
+from rest_framework.test import APIClient
+
+client = APIClient()
 
 def test_normal_addition():
     ip1 = 10
@@ -14,3 +20,16 @@ def test_bad_addition():
     act_res = add(ip1, ip2)
     exp_res = 40
     assert act_res != exp_res
+
+def test_list_sms():
+    res = client.get('/identify/')
+    assert res.status_code == 200
+
+@pytest.mark.django_db
+def test_create_sms():
+    sms = { 
+        "number" : "8767575",
+        "msg" : "hey hi"
+    }
+    res = client.post('/identify/', sms, format='json')
+    assert res.status_code == 201
