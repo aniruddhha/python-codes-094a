8f9d2da6d1c422583353121a3dbd0bc50c7b12b1
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-02 15:55:11 +0530

    mongo : agrregation

--- 10_day/adv-queries.js
+++ 10_day/adv-queries.js
@@ -15,6 +15,26 @@ db.employee.insertMany([
     { nm : 'eft', sal : 89, isPerm : true, role : 'guard', jd : ISODate('1998-01-01') },
 ])
 
+// to find all the employees, who joined after 2012-01-01
+db.employee.find({ jd : { $gte : ISODate('2012-01-01')  } })
+
+// find all the employees,who are working for company more than 5 years and less than 10 years 
+db.employee.aggregate([
+    {
+        $addFields : {
+            yrs : { 
+                $dateDiff : { startDate : '$jd', endDate : '$$NOW', unit : 'year' } 
+            } 
+        }
+    },
+    {
+        $match :  {
+            yrs : { $gte : 5, $lt : 10 }
+        }
+    }
+])
+
+
 // role wise salary
 
 db.employee.aggregate([
@@ -27,3 +47,24 @@ db.employee.aggregate([
         }  
     }
 ])
+
+db.employee.find().sort( { 'role' : 1 }).limit(1)
+
+db.employee.aggregate([
+    { 
+        //stage 2
+         $group : { 
+             _id : '$role',
+             count : { 
+                 $sum : '$sal'
+             }  
+         }  
+    },
+    {
+        // stage 1
+        $match : { count : { $gte : 90 } }
+    },
+    {
+        $sort: { count : -1 }
+    }
+ ])
