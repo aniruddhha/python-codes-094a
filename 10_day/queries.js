679ae75ae55fe5a22a24e1188c37ad970543943f
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-02 12:11:49 +0530

    mongo : update and find in details

--- 10_day/queries.js
+++ 10_day/queries.js
@@ -14,3 +14,34 @@ db.items.insertMany([
     { price : 30, qty : 56, name : 'xyz' },
     { price : 10, qty : 1056, name : 'ghn' },
 ])
+
+db.items.insertOne({
+    _id : 67,
+    price : 45, 
+    qty : 19, 
+    name : 'zzz'
+})
+
+db.items.find({ name : { '$regex' : /^lm/ } })
+db.items.find({ name : /lm/ } )
+
+db.items.find({ 
+    _id : 67
+ })
+
+db.items.find({
+    _id : ObjectId("63620309a37a09d0505b937f")
+}, { 
+    _id : 0,
+    sales : 1, 
+    dept : 1
+})
+
+db.items.updateOne(
+    { _id : ObjectId("63620309a37a09d0505b937f") },
+    { 
+        $set : { 
+            dept : 'eng' 
+        }
+    }
+)
