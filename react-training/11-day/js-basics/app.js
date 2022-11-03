52ec156d210eb21f2940699c2f117de491f0260c
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-03 12:52:04 +0530

    js : var, let, const

---
+++ react-training/11-day/js-basics/app.js
@@ -0,0 +1,14 @@
+
+// older javascript
+var a = 10
+// es6
+let b = 20
+
+const c = 30 // it is constant and behaves like let
+sagdsflkgjdflg = 90 // ❌ do not use this
+
+
+function add(num1, num2) {
+
+    return num1 + num2
+}
---
+++ react-training/11-day/js-basics/variables.js
@@ -0,0 +1,9 @@
+
+let age = 10
+// age = 20
+
+if(true) {
+    age = 90
+    // let age = 20
+}
+console.log('Age is ' + age)
