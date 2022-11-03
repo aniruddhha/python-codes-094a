2c6667a1e21c77da368a503082e0aa99c4264815
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-03 14:55:23 +0530

    javascript : conditionals

---
+++ react-training/11-day/js-basics/conditionals.js
@@ -0,0 +1,45 @@
+const day = 2
+
+
+function checkUsingIfElse() {
+    if(day == 1) {
+        console.log('mon')
+    }
+    else if (day == 2) {
+        console.log('tue')
+    }
+    else if (day == 3) {
+        console.log('wed')
+    }
+    else {
+        console.log('sun')
+    }
+    
+}
+
+function checkUsingSwitchCase() {
+    switch(day) {
+
+        case 1 : {
+            console.log('mon')
+            break
+        }
+        case 2 : {
+            console.log('tue')
+            break
+        }
+        case 3 : {
+            console.log('wed')
+            break
+        }
+        default : {
+            console.log('sun')
+        }
+    
+    }
+}
+
+checkUsingIfElse()
+checkUsingSwitchCase()
+
+
