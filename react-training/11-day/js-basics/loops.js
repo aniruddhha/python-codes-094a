68e7d667c5280d3f86222b0bc7a470ef436d6704
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-03 14:45:16 +0530

    javascript : loops

---
+++ react-training/11-day/js-basics/loops.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+    console.log(`i is ${i}`)
+}
+
+let j = 10
+while (j > 0) {
+    console.log(`j is ${j}`)
+    j--
+}
+
+let k = 10
+do {
+    console.log(`k is ${k}`)
+    k = k - 1
+} while (k > 0)
