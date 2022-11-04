6ed57f7947a6084ef16f963157537d94ac165786
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-04 11:27:04 +0530

    react : event listeners

--- react-training/12_day/firstapp/src/index.js
+++ react-training/12_day/firstapp/src/index.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 
 const dv = document.getElementById('root')
 
 const root = ReactDOM.createRoot(dv);
-const color  = 'yellow'
+// const color  = 'yellow'
 
-const dvSt = {
-  boder : '1px solid red',
-  color : 'red'
+// const dvSt = {
+//   boder : '1px solid red',
+//   color : 'red'
+// }
+
+
+
+// const uiAni =  (
+//   <div style={dvSt} >
+//     <h1>Welcome to react, Good One {color.toUpperCase()}</h1>
+//   </div>
+// )
+
+/**
+ *  - First identify click on button
+ *  - change the value of color variable
+ *  - assign the style to div with color variable 
+ * 
+ */
+
+let col = 'red'
+const onClk = () =>  {
+  col = 'yellow'
+  console.log(col)
 }
 
-const uiAni =  (
-  <div style={dvSt} >
-    <h1>Welcome to react, Good One {color.toUpperCase()}</h1>
-  </div>
-)
+const boxColorUi = <div>
+  <div className='box' style={{backgroundColor : col}}></div>
+  {/* <input type='button' value='Okay' onClick={() => console.log('clicked')}/> */}
+  <input type='button' value='Okay' onClick={onClk}/>
+</div>
 
-root.render(uiAni);
+
+root.render(boxColorUi);
 
 
