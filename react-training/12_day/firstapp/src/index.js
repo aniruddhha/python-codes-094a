dc544c91c9deab9db552c91841e9b5b8bf911d7e
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-04 09:51:19 +0530

    react : create react app

---
+++ react-training/12_day/firstapp/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>Welcome to react, Good One</h1>
+);
+
+
