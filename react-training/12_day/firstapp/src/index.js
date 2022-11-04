705fe89d37a36294e13f05b3c698e28ad002304b
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-04 11:03:27 +0530

    react : jsx intro

--- react-training/12_day/firstapp/src/index.js
+++ react-training/12_day/firstapp/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>Welcome to react, Good One</h1>
-);
+const dv = document.getElementById('root')
+
+const root = ReactDOM.createRoot(dv);
+const color  = 'yellow'
+
+const dvSt = {
+  boder : '1px solid red',
+  color : 'red'
+}
+
+const uiAni =  (
+  <div style={dvSt} >
+    <h1>Welcome to react, Good One {color.toUpperCase()}</h1>
+  </div>
+)
+
+root.render(uiAni);
 
 
