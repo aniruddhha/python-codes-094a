52084cd353c70edb330f5385e85d0f8337707076
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-07 12:28:44 +0530

    casetudy : css setup

--- react-training/casestudy/walletfront/src/login/login.js
+++ react-training/casestudy/walletfront/src/login/login.js
@@ -1,9 +1,15 @@
 import styles from './login.module.css'
 
 export function Login() {
+
+    console.log(styles)
+
     return (
-        <div className={styles.error}>
-            Hi, How are you ?
+        <div>
+            <h4>Welcome Ecommerce Wallet</h4>  
+            <input type='text' placeholder='Username'/>
+            <input type='text' placeholder='Password'/>
+            <input type='button' value='Login'/>
         </div>
     )
 }
