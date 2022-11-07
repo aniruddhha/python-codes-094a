cdabf2d167167592ac833c8802d67f257b9002c8
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-07 12:04:38 +0530

    casestudy : basics of css modules

---
+++ react-training/casestudy/walletfront/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Login } from './login/login';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Login/>
+);
+
+
---
+++ react-training/casestudy/walletfront/src/login/login.js
@@ -0,0 +1,9 @@
+import styles from './login.module.css'
+
+export function Login() {
+    return (
+        <div className={styles.error}>
+            Hi, How are you ?
+        </div>
+    )
+}
