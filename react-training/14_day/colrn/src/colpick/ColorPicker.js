85c4fbff16d196fc06542c9e110fba6f878c2274
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-09 12:27:41 +0530

    react : inherirtance vs composition

--- react-training/14_day/colrn/src/colpick/ColorPicker.js
+++ react-training/14_day/colrn/src/colpick/ColorPicker.js
@@ -2,37 +2,44 @@ import sty from './ColorPicker.module.css'
 
 import { useState } from 'react'
 
+
+export function ColorRange({ col }) {
+
+    return (
+        <div className={sty.mr}>
+            {col} <input type='range'  min={0} max={255}/> 
+        </div>
+    )
+}
+
 export function ColorPicker() {
 
-    const [red, setRed] = useState(0)
+    const arr = useState(0)
     const [green, setGreen] = useState(0)
     const [blue, setBlue] = useState(0)
 
-    const onRedChanged = (e) => setRed(Number.parseInt(e.target.value))
+    const onRedChanged = (e) => {
+        arr[1](Number.parseInt(e.target.value))
+    }
     const onGreenChanged = (e) => setGreen(Number.parseInt(e.target.value))
     const onBlueChanged = (e) => setBlue(Number.parseInt(e.target.value))
 
-    const obj = {
-        backgroundColor : `rgb(${red}, ${green}, ${blue})`
-    }
-
     return(
         <div className={sty.cont}>
-            <div 
-                className={sty.colDv} 
-                style={{ backgroundColor : `rgb(${red}, ${green}, ${blue})` }}>
-
-            </div>
+           
+            <ColorRange col='red'/>
+            <ColorRange col='green'/>
+            <ColorRange col='blue'/>
             
-            <div className={sty.mr}>
-                Red <input type='range' onChange={onRedChanged}  min={0} max={255}/> {red}
+            {/* <div className={sty.mr}>
+                Red <input type='range' onChange={onRedChanged}  min={0} max={255} value={arr[0]}/> {arr[0]}
             </div>
             <div className={sty.mr}>
-                Green <input type='range' onChange={onGreenChanged} min={0} max={255}/> {green}
+                Green <input type='range' onChange={onGreenChanged} min={0} max={255} value={green}/> {green}
             </div>
             <div className={sty.mr}>
-                Blue <input type='range' onChange={onBlueChanged} min={0} max={255}/> {blue}
-            </div>
+                Blue <input type='range' onChange={onBlueChanged} min={0} max={255} value={blue}/> {blue}
+            </div> */}
         </div>
     )
 }
