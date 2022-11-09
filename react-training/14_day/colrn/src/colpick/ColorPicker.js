347d6b1e944337cd37bf9ee4627631b83d8eac03
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-09 10:23:28 +0530

    react : color picker

--- react-training/14_day/colrn/src/colpick/ColorPicker.js
+++ react-training/14_day/colrn/src/colpick/ColorPicker.js
@@ -1,18 +1,37 @@
 import sty from './ColorPicker.module.css'
 
+import { useState } from 'react'
+
 export function ColorPicker() {
+
+    const [red, setRed] = useState(0)
+    const [green, setGreen] = useState(0)
+    const [blue, setBlue] = useState(0)
+
+    const onRedChanged = (e) => setRed(Number.parseInt(e.target.value))
+    const onGreenChanged = (e) => setGreen(Number.parseInt(e.target.value))
+    const onBlueChanged = (e) => setBlue(Number.parseInt(e.target.value))
+
+    const obj = {
+        backgroundColor : `rgb(${red}, ${green}, ${blue})`
+    }
 
     return(
         <div className={sty.cont}>
-            <div className={sty.colDv}></div>
+            <div 
+                className={sty.colDv} 
+                style={{ backgroundColor : `rgb(${red}, ${green}, ${blue})` }}>
+
+            </div>
+            
             <div className={sty.mr}>
-                <input type='range'/>
+                Red <input type='range' onChange={onRedChanged}  min={0} max={255}/> {red}
             </div>
             <div className={sty.mr}>
-                <input type='range'/>
+                Green <input type='range' onChange={onGreenChanged} min={0} max={255}/> {green}
             </div>
             <div className={sty.mr}>
-                <input type='range'/>
+                Blue <input type='range' onChange={onBlueChanged} min={0} max={255}/> {blue}
             </div>
         </div>
     )
