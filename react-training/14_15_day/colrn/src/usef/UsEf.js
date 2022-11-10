1ab892fef3e990fefc83e31c08cb61f1397063f6
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-10 10:55:59 +0530

    safe commit

--- react-training/14_15_day/colrn/src/usef/UsEf.js
+++ react-training/14_15_day/colrn/src/usef/UsEf.js
@@ -23,13 +23,33 @@ export function UsEf() {
 
     useEffect(() => {
         console.log('I would be getting called only once')
+
+        console.log('Step 1: Begining of Timeout')
+        setTimeout( () => console.log('Step 2: 1s Timeout Happened'), 1000)
+        console.log('Step 3: End of Timeout')
+        console.log('Step 4: End of Timeout')
+        console.log('Step 5: End of Timeout')
+
+        setTimeout( () => console.log('Step 34: 5s Timeout Happened'), 1000)
+        console.log('Step 6: End of Timeout')
+        console.log('Step 7: End of Timeout')
+        console.log('Step 8: End of Timeout')
+        console.log('Step 9: End of Timeout')
+        console.log('Step 10: End of Timeout')
+        console.log('Step 11: End of Timeout')
+
+        //
+        //
+        //
+        // 
+
     }, [])
 
     useEffect(() => {
         console.log('I would be getting called only if ab changes')
     }, [ab])
 
-    useEffect( () => {
+    useEffect(() => {
 
         function onResize() {
             setWd(window.innerWidth)
@@ -40,19 +60,27 @@ export function UsEf() {
 
         return () => {
             // cleanup
-            window.removeEventListener('resize')
+            window.removeEventListener('resize', onResize)
         }
+    })
 
-    } )
-   
+    useEffect( () => {
+        const prm = fetch('https://reqres.in/api/users/2')
+        .then( httpRes => httpRes.json())
+        .then(json => console.log(json))
+
+        console.log(prm)
+
+    }, [])
+
     return (
         <>
             <div>
                 <h1>Width {wd}</h1>
                 <h1>Height {ht}</h1>
             </div>
-            <input type='button' value='Okay' onClick={onCl}/>
-            <input type='button' value='Ab' onClick={onAb}/>
+            <input type='button' value='Okay' onClick={onCl} />
+            <input type='button' value='Ab' onClick={onAb} />
         </>
     )
 }
