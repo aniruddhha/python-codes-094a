bbd4a500fd8801aea6ceed95f729e6b99030797a
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-14 11:29:05 +0530

    react : error boundry

--- react-training/17_day/refserrrenprp/src/refsdemo.js
+++ react-training/17_day/refserrrenprp/src/refsdemo.js
@@ -9,15 +9,10 @@ export function RefDemo({ num1, num2 }) {
         console.log(refNm2.current)
     }, []) 
 
-    const clk = () => {
-        throw new Error('Bad Data')
-    }
-
     return (
         <div>
             <input type='text' placeholder={num1.toUpperCase()}/>
             <input type='text'  placeholder={num2.toUpperCase()} ref={refNm2}/>
-            <input type='button' value='Error' onClick={clk}  />
         </div>
     )
 }
