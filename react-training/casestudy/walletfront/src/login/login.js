d503711407af176ca5e54e956c577a183c9a8134
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-15 22:01:03 +0530

    login : rest api integration with validation

--- react-training/casestudy/walletfront/src/login/login.js
+++ react-training/casestudy/walletfront/src/login/login.js
@@ -1,29 +1,77 @@
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { json, useNavigate } from 'react-router-dom'
 import styles from './login.module.css'
 
 export function Login() {
+
+    const [errMsg, setErrMsg] = useState( { sts : true, msg : 'Invalid Form'})
+
+    const [creds, setCreds] = useState({
+        user_name: '',
+        password: ''
+    })
+
+    const [er, setEr] = useState({ usNm: true, pass: true })
+
+    useEffect(() => {
+        setEr({
+            usNm: creds.user_name.length < 3,
+            pass: creds.password.length < 3
+        })
+        setErrMsg( { sts : false, msg : 'Invalid Form'})
+    }, [creds])
+
+    const onIpCh = e => setCreds({ ...creds, [e.target.name]: e.target.value })
 
     const navigate = useNavigate()
 
     const onClkLogin = () => {
 
-        navigate('/dash')
+        if (Object.values(er).every(el => el == false)) {
+
+            fetch('http://localhost:8000/login/login/', {
+                method: 'post',
+                body: JSON.stringify(creds),
+                headers : {
+                    'Content-Type' : 'application/json'
+                }
+            })
+            .then( res =>  {
+                if(!res.ok) return Promise.reject(res)
+                return res.json() 
+            })
+            .then(json => {
+                navigate('/dash')
+            })
+            .catch( errRes => {
+                errRes.json().then( e => {
+                    console.log(e)
+                   setErrMsg({ sts : true, msg :  e.err.non_field_errors[0]})
+                } )
+            })
+        } else {
+            setErrMsg( { sts : false, msg : 'Invalid Form'})
+        }
     }
 
     return (
         <div className={styles.mainDiv}>
             <span className={styles.ttl}>Login</span>
             <span className={styles.txt}>Sign In to Continue</span>
-            
+
             <div className={styles.uipDv}>
                 <label className={styles.txt}>UserName</label>
-                <input className={styles.bigIp} type='text' placeholder='Username'/>
+                <input name='user_name' className={styles.bigIp} type='text' placeholder='Username' onChange={onIpCh} />
+                {er.usNm && (<span className={styles.err}> Invalid Username</span>)}
             </div>
             <div className={styles.uipDv}>
                 <label className={styles.txt}>Password</label>
-                <input className={styles.bigIp} type='text' placeholder='Password'/>
+                <input name='password' className={styles.bigIp} type='text' placeholder='Password' onChange={onIpCh} />
+                {er.pass && (<span className={styles.err}> Invalid Password</span>)}
             </div>
-            <input className={styles.btnLgn} type='button' value='Login' onClick={onClkLogin}/>
+            <input className={styles.btnLgn} type='button' value='Login' onClick={onClkLogin} />
+
+            {(er.usNm || er.pass || errMsg.sts) && <span className={styles.alert}>{errMsg.msg}</span>}
         </div>
     )
 }
