1447ad69b3da6f54822d4ce50e04450f0cc96268
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-18 12:08:27 +0530

    react : jest : unit testing

--- react-training/21_day/testingdemo/src/calc.js
+++ react-training/21_day/testingdemo/src/calc.js
@@ -1,7 +1,12 @@
 export function addition(num1, num2) {
     if(num2 < 0) return num1
+    if(num1 < 34 || num1 >78 ) return (num1*num1) + num2
     return num1 + num2
 }
 
 
+
+
 // num2 should not be negative
+// num1 must be in the range of 34 to 78, 
+// otherwise make square of num1
--- react-training/21_day/testingdemo/src/calc.test.js
+++ react-training/21_day/testingdemo/src/calc.test.js
@@ -1,56 +1,74 @@
 import { addition } from "./calc";
 
-test('adds 1 + 3 to equal 4', () => {
+// test('adds 1 + 3 to equal 4', () => {
 
-    const expected = 4
-    const actual = addition(1 , 3)
+//     const expected = 4
+//     const actual = addition(1 , 3)
 
-    expect(actual).toBe(expected)
-})
+//     expect(actual).toBe(expected)
+// })
 
-test('1 + -1 = 1', () => {
-    const expected = 1
-    const actual = addition(1 , -1)
+// test('1 + -1 = 1', () => {
+//     const expected = 1
+//     const actual = addition(1 , -1)
 
-    expect(actual).toBe(expected)
-})
+//     expect(actual).toBe(expected)
+// })
 
-test('-1 + 3 = 2', () => {
-    const expected = 2
-    const actual = addition(-1 , 3)
+// test('-1 + 3 = 2', () => {
+//     const expected = 2
+//     const actual = addition(-1 , 3)
+    
+//     expect(actual).toBe(expected)
+// })
+
+// test('3 + 3 = 6', () => {
+//     const expected = 6
+//     const actual = addition(3 , 3)
+    
+//     expect(actual).toBe(expected)
+// })
+
+// test('0 + 0 = 0', () => {
+//     const expected = 0
+//     const actual = addition(0 , 0)
+    
+//     expect(actual).toBe(expected)
+// })
+
+// test('1 + 0 = 1', () => {
+//     const expected = 1
+//     const actual = addition(1 , 0)
+    
+//     expect(actual).toBe(expected)
+// })
+
+// test('0 + 1 = 1', () => {
+//     const expected = 1
+//     const actual = addition(0 , 1)
+    
+//     expect(actual).toBe(expected)
+// })
+
+test('2 + 35 = 39', () => {
+    const expected = 39
+    const actual = addition(2, 35)
     
     expect(actual).toBe(expected)
 })
 
-test('3 + 3 = 6', () => {
-    const expected = 6
-    const actual = addition(3 , 3)
-    
+test('80 + 2 = 6402', () => {
+    const expected = 6402
+    const actual = addition(80, 2)
+
     expect(actual).toBe(expected)
 })
 
-test('0 + 0 = 0', () => {
-    const expected = 0
-    const actual = addition(0 , 0)
-    
+test('50 + 50 = 100', () => {
+    const expected = 100
+    const actual = addition(50, 50)
     expect(actual).toBe(expected)
 })
-
-test('1 + 0 = 1', () => {
-    const expected = 1
-    const actual = addition(1 , 0)
-    
-    expect(actual).toBe(expected)
-})
-
-test('0 + 1 = 1', () => {
-    const expected = 1
-    const actual = addition(0 , 1)
-    
-    expect(actual).toBe(expected)
-})
-
-
 
 
 //calc.test.js
