95229157578666305034474962fa869e952dfa4c
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-18 11:36:34 +0530

    jest : menu and tdd approach

--- react-training/21_day/testingdemo/src/calc.js
+++ react-training/21_day/testingdemo/src/calc.js
@@ -1,3 +1,7 @@
 export function addition(num1, num2) {
+    if(num2 < 0) return num1
     return num1 + num2
 }
+
+
+// num2 should not be negative
--- react-training/21_day/testingdemo/src/calc.test.js
+++ react-training/21_day/testingdemo/src/calc.test.js
@@ -1,6 +1,5 @@
 import { addition } from "./calc";
 
-
 test('adds 1 + 3 to equal 4', () => {
 
     const expected = 4
@@ -8,6 +7,51 @@ test('adds 1 + 3 to equal 4', () => {
 
     expect(actual).toBe(expected)
 })
+
+test('1 + -1 = 1', () => {
+    const expected = 1
+    const actual = addition(1 , -1)
+
+    expect(actual).toBe(expected)
+})
+
+test('-1 + 3 = 2', () => {
+    const expected = 2
+    const actual = addition(-1 , 3)
+    
+    expect(actual).toBe(expected)
+})
+
+test('3 + 3 = 6', () => {
+    const expected = 6
+    const actual = addition(3 , 3)
+    
+    expect(actual).toBe(expected)
+})
+
+test('0 + 0 = 0', () => {
+    const expected = 0
+    const actual = addition(0 , 0)
+    
+    expect(actual).toBe(expected)
+})
+
+test('1 + 0 = 1', () => {
+    const expected = 1
+    const actual = addition(1 , 0)
+    
+    expect(actual).toBe(expected)
+})
+
+test('0 + 1 = 1', () => {
+    const expected = 1
+    const actual = addition(0 , 1)
+    
+    expect(actual).toBe(expected)
+})
+
+
+
 
 //calc.test.js
 //calc.spec.js
