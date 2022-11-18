fb2ed5bd110c42a8c0817e09f4e65bdaf27b185a
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-18 11:03:09 +0530

    react : jest : unit testing

---
+++ react-training/21_day/testingdemo/src/calc.test.js
@@ -0,0 +1,16 @@
+import { addition } from "./calc";
+
+
+test('adds 1 + 3 to equal 4', () => {
+
+    const expected = 4
+    const actual = addition(1 , 3)
+
+    expect(actual).toBe(expected)
+})
+
+//calc.test.js
+//calc.spec.js
+// calctest.js
+//testcalc.js
+// test/abc.tes
