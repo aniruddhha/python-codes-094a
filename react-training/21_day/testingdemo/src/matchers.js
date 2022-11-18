6ab3cac6decea07879c4df7f6f5abea3c3848ff4
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-11-18 12:33:32 +0530

    jest : matchers

---
+++ react-training/21_day/testingdemo/src/matchers.js
@@ -0,0 +1,20 @@
+export function giveMeObj() {
+
+    return {
+        a : 1, 
+        b : 2
+    }
+}
+
+export function giveMeNullObj() {
+
+    return null
+}
+
+export function getMeNumber() {
+    return 10
+}
+
+export function getMeString() {
+    return 'abc is abc and not pqr'
+}
---
+++ react-training/21_day/testingdemo/src/matchers.test.js
@@ -0,0 +1,34 @@
+import { 
+    giveMeNullObj, 
+    giveMeObj,
+    getMeNumber,
+    getMeString
+} from './matchers'
+
+test ('check object equality', () => {
+
+    const obj = giveMeObj()
+
+    expect(obj).toEqual({ a : 1 , b : 2 })
+})
+
+test ('check object not null', () => {
+
+    expect(giveMeObj()).not.toBeNull()
+    expect(undefined).toBeFalsy();
+    expect(null).toBeFalsy();
+    expect(0).toBeFalsy();
+    expect( 1 != 1).toBeFalsy();
+})
+
+test('check number ', () => {
+    const num = getMeNumber(); 
+    expect(num).toBeGreaterThan(5);
+    expect(num).toBe(10);
+})
+
+test('string checking', () => {
+    const str = getMeString()
+
+    expect(str).toMatch('abc is abc and not pqr')
+})
